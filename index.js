f95c5a14d72cb0b5bfe44fcf7abda974ae99f369
kenny caldieraro <kenny.caldieraro@gmail.com>
2022-05-10 22:19:37 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,49 @@
+require("dotenv").config();
+const fetch = require("node-fetch");
+const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+const WPAPI = require("wpapi");
+const wp = new WPAPI({
+  endpoint: process.env.ENDPOINT,
+  username: process.env.USERNAME,
+  password: process.env.PASSWORD,
+});
+
+let exclude = process.env.DATE;
+async function checkThumbnails() {
+  let url = `${process.env.CHECKURL + process.env.MODE + exclude}`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      for (let i = 1; i < data.length; i++) {
+        if (!data[i]._embedded["wp:featuredmedia"]) {
+          return console.log("work done");
+        } else {
+          if (!UrlExists(data[i]._embedded["wp:featuredmedia"][0].source_url)) {
+            wp.posts()
+              .id(data[i].id)
+              .delete()
+              .then((data) => {
+                console.log("remove id= " + data.id);
+              })
+              .catch((err) => {
+                console.log(err);
+              });
+          } else {
+            exclude = data[i].date;
+            console.log("ok " + data[i].id);
+          }
+        }
+      }
+      checkThumbnails();
+    });
+}
+
+async function UrlExists(url) {
+  let http = new XMLHttpRequest();
+  http.open("HEAD", url, false);
+  http.send();
+  if (http.status != 404) return true;
+  else return false;
+}
+
+checkThumbnails();
---
+++ readme.md
@@ -0,0 +1,7 @@
+# A small script to remove video without thumbnail on wordpress
+
+Check the .env file to configure the script.
+
+I use after/before to skip media already processed with ISO date. (for starting the date is "0000-00-00 00:00:00").
+
+Is necessary use the plugin wordpress Basic-Auth-master to setup a simple authentication.
